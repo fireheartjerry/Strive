c876e10915d4b4c9f81fcceef9bf9f90cc429fc5
Jerry Li <fireheartjerry@gmail.com>
2025-06-20 22:18:00 -0400

    Add skeleton api

---
+++ api.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Register API endpoints without decorators
+
+def ping():
+    return jsonify({'message': 'pong'})
+
+app.add_url_rule('/ping', 'ping', ping, methods=['GET'])
+
+# Example POST endpoint
+
+def echo():
+    data = request.get_json() or {}
+    return jsonify({'you_sent': data})
+
+app.add_url_rule('/echo', 'echo', echo, methods=['POST'])
+
+# New GET endpoint for glebber
+def glebber():
+    return jsonify({'message': 'glebber'})
+
+app.add_url_rule('/glebber', 'glebber', glebber, methods=['GET'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
